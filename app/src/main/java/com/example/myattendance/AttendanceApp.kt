c6b71376cc57113137c1f249aa925a96080f6d87
arfath <md8arfath@gmail.com>
2024-01-12 15:26:48 +0530

    app run

---
+++ app/src/main/java/com/example/myattendance/AttendanceApp.kt
@@ -0,0 +1,15 @@
+package com.example.myattendance
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AttendanceApp(){
+
+    Text(
+        text = "Hello arfath here ",
+        modifier = Modifier
+    )
+
+}
