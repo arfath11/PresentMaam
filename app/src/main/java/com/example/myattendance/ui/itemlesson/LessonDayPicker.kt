5f16c5271dc3f3d8d748fc082660d3dfb933ff4c
arfath <md8arfath@gmail.com>
2024-02-06 17:03:47 +0530

    "start and end date"

---
+++ app/src/main/java/com/example/myattendance/ui/itemlesson/LessonDayPicker.kt
@@ -0,0 +1,14 @@
+package com.example.myattendance.ui.itemlesson
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LessonDayPicker(modifier: Modifier = Modifier){
+
+    Row(horizontalArrangement = Arrangement.SpaceBetween){
+
+    }
+}
--- app/src/main/java/com/example/myattendance/ui/itemlesson/LessonEntryScreen.kt
+++ app/src/main/java/com/example/myattendance/ui/itemlesson/LessonEntryScreen.kt
@@ -2,6 +2,8 @@ package com.example.myattendance.ui.itemlesson
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +14,9 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,12 +24,14 @@ import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,7 +43,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myattendance.R
+import com.example.myattendance.feature.extension.toFormattedDateString
 import com.example.myattendance.ui.theme.MyAttendanceTheme
+import java.util.Calendar
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,24 +66,22 @@ fun AttendanceBottomSheet(onDismissRequest: () -> Unit) {
 fun ItemEntry(modifier: Modifier= Modifier) {
 
     var subjectName by remember { mutableStateOf("") }
-    var minPer by remember { mutableStateOf("") }
+    var minPer by remember { mutableStateOf("75") }
     var save by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
             .padding(16.dp)
             .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = stringResource(R.string.add_subject),
             style = MaterialTheme.typography.headlineMedium,
             modifier = Modifier
-                    .padding(bottom = 16.dp)
-                    .align(alignment = Alignment.Start)
+                .padding(bottom = 16.dp)
+                .align(alignment = Alignment.Start)
         )
-
 
         Row(
             modifier = modifier
@@ -82,7 +89,7 @@ fun ItemEntry(modifier: Modifier= Modifier) {
                // .size(400.dp),
         ){
 
-
+            //To Enter Subject percentage
             EditNumberField(
                 label = R.string.subject_name,
                 keyboardOptions = KeyboardOptions.Default.copy(
@@ -91,13 +98,15 @@ fun ItemEntry(modifier: Modifier= Modifier) {
                 ),
                 value = subjectName ,
                 onValueChanged = { subjectName = it },
-                modifier = Modifier.padding(bottom = 32.dp, end = 16.dp).fillMaxWidth(0.76f),
+                modifier = Modifier
+                    .padding(bottom = 32.dp, end = 16.dp)
+                    .fillMaxWidth(0.76f),
             )
-
+            //To Enter Min percentage
             EditNumberField(
                 label = R.string.min_attendnce,
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Text,
+                    keyboardType = KeyboardType.Number,
                     imeAction = ImeAction.Next
                 ),
                 value = minPer ,
@@ -107,10 +116,87 @@ fun ItemEntry(modifier: Modifier= Modifier) {
         }
 
 
+            LessonDateTextField(endDate = {},
+                                label = R.string.start_date,
+                                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            LessonDateTextField(endDate = {},
+                                label = R.string.end_date,
+                                modifier = Modifier.padding(bottom = 32.dp)
+            )
+           // LessonDateTextField(endDate = {})
+
 
     }
 }
 
+
+@Composable
+fun LessonDayTextField(){
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LessonDateTextField(endDate: (Long) -> Unit,
+                        @StringRes label: Int,
+                        modifier: Modifier = Modifier) {
+
+    Text(
+        text = stringResource(id = label),
+        style = MaterialTheme.typography.titleSmall,
+        modifier= Modifier.padding(bottom = 4.dp)
+    )
+
+
+    var shouldDisplay by remember { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed: Boolean by interactionSource.collectIsPressedAsState()
+    if (isPressed) {
+        shouldDisplay = true
+    }
+
+    val today = Calendar.getInstance()
+    today.set(Calendar.HOUR_OF_DAY, 0)
+    today.set(Calendar.MINUTE, 0)
+    today.set(Calendar.SECOND, 0)
+    today.set(Calendar.MILLISECOND, 0)
+    val currentDayMillis = today.timeInMillis
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = System.currentTimeMillis(),
+
+    )
+
+    var selectedDate by rememberSaveable {
+        mutableStateOf(
+            datePickerState.selectedDateMillis?.toFormattedDateString() ?: ""
+        )
+    }
+
+
+    LessonDatePickerDialog(
+        state = datePickerState,
+        shouldDisplay = shouldDisplay,
+        onConfirmClicked = { selectedDateInMillis ->
+            selectedDate = selectedDateInMillis.toFormattedDateString()
+            endDate(selectedDateInMillis)
+        },
+        dismissRequest = {
+            shouldDisplay = false
+        }
+    )
+
+    TextField(
+        modifier = modifier,
+        readOnly = true,
+        value = selectedDate,
+        onValueChange = {},
+        trailingIcon = { Icon(imageVector = Icons.Default.DateRange, contentDescription ="date range") },
+        label = {stringResource(id = label)},
+        interactionSource = interactionSource
+    )
+}
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
@@ -124,7 +210,7 @@ fun EditNumberField(
         value = value,
         singleLine = true,
        // leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
-        modifier = modifier.clip(shapes.medium),
+        modifier = modifier,
         onValueChange = onValueChanged,
         label = { Text(stringResource(label)) },
         keyboardOptions = keyboardOptions
